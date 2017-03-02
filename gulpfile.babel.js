dc40d6459d87dbeb4265517cadc2dac1ee574bc5
gattadesmond <gattadesmond@gmail.com>
2017-03-02 15:10:59 +0700

    Sua loi gulp build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -183,7 +183,7 @@ function sass() {
 function javascript() {
   return gulp.src(PATHS.javascript)
     // .pipe($.sourcemaps.init())
-    // .pipe($.babel({ignore: ['what-input.js']}))
+    .pipe($.babel({ignore: ['what-input.js']}))
     .pipe($.concat('app.js'))
     .pipe($.if(PRODUCTION, $.uglify()
       .on('error', e => { console.log(e); })
